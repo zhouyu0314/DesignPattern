91fcd4b6a18a47c504a548ec6c824a213a85078f
zhouyu0314 <181332969@qq.com>
2020-12-09 17:41:35 +0800

    1209

--- src/main/java/com/factoryPattern/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.factoryPattern;
-
-public interface Shape {
-   void draw();
-}
---
+++ src/main/java/com/factoryPattern/idcard/IdCardFactory.java
@@ -0,0 +1,30 @@
+package com.factoryPattern.idcard;
+
+import com.factoryPattern.framework.Factory;
+import com.factoryPattern.framework.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 具体创建者
+ * 负责生成具体产品
+ */
+public class IdCardFactory extends Factory {
+    private List owners = new ArrayList();
+
+
+    @Override
+    protected Product createProduct(String owner) {
+        return new IdCard(owner);
+    }
+
+    @Override
+    protected void registerProduct(Product product) {
+        owners.add(((IdCard)product).getOwner());
+    }
+
+    public List getOwners(){
+        return owners;
+    }
+}
